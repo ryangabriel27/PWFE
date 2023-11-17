2f4bf5a993c5183ecb3c43e0b63e334e1c6a02e5
ryangabriel27 <ryanjulio56@gmail.com>
2023-11-17 14:37:28 -0300

    17.11

---
+++ Vue/projeto-clima-api/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <Clima/>
+  </div>
+</template>
+<script>
+import Clima from './components/Clima.vue';
+export default {
+  name: 'App',
+  components: {
+    Clima,
+  },
+};
+</script>
